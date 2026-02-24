dc8fc95ad4ea23e98eb8635033c8846862b4ea43
Md Arib Hussain <hussainarib839@gmail.com>
2026-02-24 15:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (4.85%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,13 @@
+int search(int* nums, int numsSize, int target) {
+    int i = 0, j = numsSize-1, mid;
+    while(i<=j){
+        mid = (i + j)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if(target < nums[mid])
+            j = mid-1;
+        else
+            i = mid+1;
+    }
+    return -1;
+}
