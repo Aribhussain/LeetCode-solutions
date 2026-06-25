aa4abc4b30572c804824770b29bf2adad5ed75ea
Md Arib Hussain <hussainarib839@gmail.com>
2026-06-25 22:54:17 +0530

    Time: 13 ms (39.45%) | Memory: 32 MB (85.13%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        std::sort(nums.begin(),nums.end());
+        int sum = 0;
+        for(int i = 0;i < nums.size();i+=2){
+            if(i%2 == 0){
+                sum = sum + nums[i];
+            }
+        }
+        return sum;    
+    }
+};
